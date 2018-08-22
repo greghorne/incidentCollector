8de4c3e87958a2cde6a7d52237d8cddec27dc8d3
greghorne <greghorne@hotmail.com>
2018-08-22 23:28:37 +0000

    updated code

--- incidentCollector.rb
+++ incidentCollector.rb
@@ -43,8 +43,6 @@ def self.get_db_conn()
         pp e
         return false
     end
-
-
 end
 
 # ------------------------------------------------------------
@@ -57,58 +55,28 @@ $db_port = ENV['RAILS_PORT']
 $db_user = ENV['RAILS_USER']
 $db_pwd  = ENV['RAILS_PASSWORD']
 
-# pp server
-# pp database
-# pp port
-# pp user
-# pp password
-
-
 while true
     
     content = open(http).read
     json = JSON.parse(content)
 
     conn = get_db_conn()
-    pp conn
 
-
-
+    counter = 0
     json['Incidents']['Incident'].each do |child|
 
-        str = "insert into tfd_incidents values('" + child['IncidentNumber'].to_s + "','" + child['Problem'].to_s + "','" + child['Address'].to_s + "','" + child['ResponseDate'].to_s + "','" + child['Latitude'].to_s + "','" + child['Longitude'].to_s + "','" + child['Vehicles']['Vehicle'].to_s + "')"
-        str = "insert into tfd_incidents values('" + child['IncidentNumber'].to_s + "','" + child['Problem'].to_s + "','" + child['Address'].to_s + "','" + child['ResponseDate'].to_s + "','" + child['Latitude'].to_s + "','" + child['Longitude'].to_s + "','"Greg"')"
-        pp str
-        puts
-        response_insert = conn.query(str)
-                      
-                    #   insert into KEYS (name, value) values (
-                    #     'blah', 'true') on conflict (name) do nothing;
-
+        response_query = conn.query("select * from tfd_incidents where IncidentNumber = '" + child['IncidentNumber'] + "'")
+        if response_query.cmd_tuples === 0
+            strJSON =  child['Vehicles'].to_json
+            str = "insert into tfd_incidents values('" + child['IncidentNumber'].to_s + "','" + child['Problem'].to_s + "','" + child['Address'].to_s + "','" + child['ResponseDate'].to_s + "','" + child['Latitude'].to_s + "','" + child['Longitude'].to_s + "','" + strJSON + "')"
+            response_insert = conn.query(str)
+            counter = counter + 1
+        end
     end
 
     conn.close
+    puts "Records Inserted: " + counter.to_s
 
     sleep 60
     puts "---------------"
 end
-
-    #     pp child['IncidentNumber']
-    #     pp child['Problem']
-    #     pp child['Address']
-    #     pp child['ResponseDate']
-    #     pp child['Latitude']
-    #     pp child['Longitude']
-    #     pp child['Vehicles']
-    #     puts "---------------"
-
-# create table tfd_incidents(
-#     IncidentNumber text primary key not null,
-#     Problem text not null,
-#     Address text not null,
-#     ResponseDate text not null,
-#     Latitude text not null,
-#     Longitude text not null,
-#     Vehicles json not null
-# )
-
