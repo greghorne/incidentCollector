5e494715e9a8035485ecd88fe0d3827da7b49095
greghorne <greghorne@hotmail.com>
2018-08-22 21:07:55 +0000

    code update

--- incidentCollector.rb
+++ incidentCollector.rb
@@ -2,6 +2,7 @@
 
 require 'open-uri'
 require 'json'
+require 'pg'
 require 'pp'
 
 def getJSON()
@@ -27,9 +28,28 @@ while true
     content = open(http).read
     json = JSON.parse(content)
 
-    pp json
+    json['Incidents']['Incident'].each do |child|
+        pp child['IncidentNumber']
+        pp child['Problem']
+        pp child['Address']
+        pp child['ResponseDate']
+        pp child['Latitude']
+        pp child['Longitude']
+        pp child['Vehicles'].
+        puts "---------------"
+    end
 
-    sleep 10
+    sleep 60
     puts "---------------"
 end
+
+# create table tfd_incidents(
+#     IncidentNumber text primary key not null,
+#     Problem text not null,
+#     Address text not null,
+#     ResponseDate text not null,
+#     Latitude text not null,
+#     Longitude text not null,
+#     Vehicles json not null
+# )
 
